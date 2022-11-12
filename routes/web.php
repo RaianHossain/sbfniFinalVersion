d10ddd22dcd59a70a7e8dde5bc92f429a7e40e93
santo1989 <santo.botany@gmail.com>
2022-11-12 13:02:07 +0600

    php artisan command include in web

--- routes/web.php
+++ routes/web.php
@@ -277,9 +277,6 @@ Route::middleware('auth')->group(function () {
     //greetingmessages
 
     Route::resource('/admin/greetingmessages', GreetingMessageController::class);
-
-    
-
 });
 
 Route::resource('/admin/message', MessageController::class);
@@ -291,6 +288,49 @@ Route::get('/notification/{message}/{notification}', [NotificationController::cl
 require __DIR__ . '/auth.php';
 
 
+//php artisan command
+
 Route::get('/foo', function () {
     Artisan::call('storage:link');
+    echo "storage link created";
+});
+
+Route::get('/cleareverything', function () {
+    $clearcache = Artisan::call('cache:clear');
+    echo "Cache cleared<br>";
+
+    $clearview = Artisan::call('view:clear');
+    echo "View cleared<br>";
+
+    $clearconfig = Artisan::call('config:cache');
+    echo "Config cleared<br>";
+});
+
+Route::get('/key =', function () {
+    $key =  Artisan::call('key:generate');
+    echo "key:generate<br>";
+});
+
+Route::get('/migrate', function () {
+    $migrate = Artisan::call('migrate');
+    echo "migration create<br>";
+});
+
+Route::get('/migrate-fresh', function () {
+    $fresh = Artisan::call('migrate:fresh --seed');
+    echo "migrate:fresh --seed create<br>";
+});
+
+Route::get('/optimize', function () {
+    $optimize = Artisan::call('optimize:clear');
+    echo "optimize cleared<br>";
+});
+Route::get('/route-clear', function () {
+    $route_clear = Artisan::call('route:clear');
+    echo "route cleared<br>";
+});
+
+Route::get('/route-cache', function () {
+    $route_cache = Artisan::call('route:cache');
+    echo "route cache<br>";
 });
